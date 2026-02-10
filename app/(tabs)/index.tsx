d2855dafa34a5385ae6bdf7cd5b969ecd81eefb8
Ahmad Faraz Nazar <ahmadfaraznazar@gmail.com>
2026-02-11 01:14:04 +0500

    add initial routes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
 import { Text, View } from 'react-native';
-
+import { Link } from 'expo-router';
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-5xl text-primary font-bold">Hello Worlds !</Text>
+      <Link href="/movie/[id]">
+        <Text>Movie 1</Text>
+      </Link>
     </View>
   );
 }
