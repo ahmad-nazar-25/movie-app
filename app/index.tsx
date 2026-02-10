837e89e7795b5c558fad120bf19bca9f8f0aeb08
Ahmad Faraz Nazar <ahmadfaraznazar@gmail.com>
2026-02-11 00:15:15 +0500

    fix nativewind setup

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text>Hello World!</Text>
+      <Text className="text-5xl text-blue-500 font-bold">Hello Worlds !</Text>
     </View>
   );
 }
